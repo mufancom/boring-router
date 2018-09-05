d9e724c3e8ef3c3ea9884b04c03953cb9443d8b8
vilic <i@vilic.info>
2018-09-05 22:28:52 +0800

    Add <NavLink>

--- src/library/index.ts
+++ src/library/index.ts
@@ -3,4 +3,5 @@ export * from './router';
 export * from './route-match';
 export * from './route';
 export * from './link';
+export * from './nav-link';
 export * from './redirect';
---
+++ src/library/nav-link.tsx
@@ -0,0 +1,32 @@
+import classNames from 'classnames';
+import {observer} from 'mobx-react';
+import React, {Component, ReactNode} from 'react';
+
+import {Link, LinkProps} from './link';
+import {RouteMatch} from './route-match';
+
+export interface NavLinkProps<TRouteMatch extends RouteMatch>
+  extends LinkProps<TRouteMatch> {
+  exact?: boolean;
+  activeClassName?: string;
+}
+
+@observer
+export class NavLink<TRouteMatch extends RouteMatch> extends Component<
+  NavLinkProps<TRouteMatch>
+> {
+  render(): ReactNode {
+    let {className, activeClassName = 'active', exact, ...props} = this.props;
+
+    let {to} = props;
+
+    let matched = exact ? to.$exact : to.$matched;
+
+    return (
+      <Link
+        className={classNames(className, matched && activeClassName)}
+        {...props}
+      />
+    );
+  }
+}
